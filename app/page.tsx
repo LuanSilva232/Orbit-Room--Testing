bc1b7eae9785658e08690be89e20eaa5ee348cb2
LuanSilva232 <rieieek2@gmail.com>
2026-08-19 17:10:19 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
-lassName="h-screen text-slate-100">
+import { ShareRoom } from '@/components/share-room'
+
+export default function Home() {
+  return (
+    <div className="h-screen text-slate-100">
       <ShareRoom />
     </div>
+  )
+}
