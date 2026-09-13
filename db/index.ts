d8a1c59ec37ac632133f50d8cfc3bca7b8aa43e3
LuanSilva232 <rieieek2@gmail.com>
2026-09-13 03:37:40 -0300

    Update index.ts

--- db/index.ts
+++ db/index.ts
@@ -73,10 +73,6 @@ export function ensureDb(): Promise<void> {
           ADD COLUMN IF NOT EXISTS device text
       `
       await sql`
-        ALTER TABLE rtc_clients
-          ADD COLUMN IF NOT EXISTS mic_muted boolean NOT NULL DEFAULT false
-      `
-      await sql`
         CREATE TABLE IF NOT EXISTS rtc_mailbox (
           id        bigserial PRIMARY KEY,
           to_client text NOT NULL,
